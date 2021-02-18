ca11a113a1cfff1b9e24cc3579e89630cf1bca53
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-02-18 22:29:36 +0100

    Fixed time zone issue

--- Code/script.js
+++ Code/script.js
@@ -1,8 +1,10 @@
 // All the DOMs are here
+const selectCity = document.getElementById("select-city")
 const cityName = document.getElementById("city-name")
 const tempCelsius = document.getElementById("tempCelsius")
 const sunSet = document.getElementById("sunSet")
 const sunRise = document.getElementById("sunRise")
+const funnyText = document.getElementById("funny-text")
 const highlight = document.getElementById("highlight")
 const container = document.getElementById("container")
 const dayOne = document.getElementById("dayOne")
@@ -13,23 +15,26 @@ const body = document.querySelector(".body")
 
 
 // Global Variable
-const OurAPI = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Stockholm&cnt=10&appid=886705b4c1182eb1c69f28eb8c520e20&units=metric"
+//const OurAPI = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Stockholm&cnt=10&appid=886705b4c1182eb1c69f28eb8c520e20&units=metric"
 //http://api.openweathermap.org/data/2.5/forecast?q=Stockholm&units=metric&appid=3c8d0ca53cf60cf5802dc4c0325edd88
 
 
-const SthlmTemp = () => {
-    fetch(OurAPI).then((response) => {
+const SthlmTemp = (userChoice) => {
+  const cityAPI = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${userChoice}&cnt=10&appid=886705b4c1182eb1c69f28eb8c520e20&units=metric` 
+  fetch(cityAPI).then((response) => {
         return response.json();
     }).then((json) => {
-        //Sunset time
-        const unixSet = json.list[0].sunset;
-        //Calculation to convert unix stamp to normal timezone
-        const dateSet = new Date(unixSet*1000).toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
-  
+        //Timezone
+        const TimeZone = json.city.timezone;
+        const timezoneOffSet = new Date().getTimezoneOffset()*60;
         //Sunrise time
-        const unixRise = json.list[0].sunrise;
-        //Calculation to convert unix stamp to normal timezone
-        const dateRise = new Date(unixRise*1000).toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
+        const sunRiseUnix = json.list[0].sunrise;
+        const sunRiseDate = new Date((sunRiseUnix + TimeZone + timezoneOffSet)*1000);
+        sunRise.innerHTML = `Sunrise: ${sunRiseDate.getHours()}:${sunRiseDate.getMinutes()}`  
+        //Sunset time
+        const sunSetUnix = json.list[0].sunset;
+        const sunSetDate = new Date((sunSetUnix + TimeZone + timezoneOffSet)*1000)
+        sunSet.innerHTML = `Sunset: ${sunSetDate.getHours()}:${sunSetDate.getMinutes()}`
         
         //Min and Max temperature
         const minTemperature = Math.round(json.list[0].temp.min);
@@ -47,7 +52,6 @@ const SthlmTemp = () => {
         const temperatureFiveDays = temperatureArrayDaysRounded.filter((day, index) => {
           return index > 0 && index < 6
         });
-        console.log(temperatureFiveDays);
         //Temperature current day
         const tempertureCurrentDay = temperatureArrayDaysRounded[0];
         
@@ -92,9 +96,7 @@ const SthlmTemp = () => {
       // Adding API information into HTML elements 
         highlight.innerHTML = ` ${currentDayWeatherDescrition} <img width="40px" src="http://openweathermap.org/img/wn/${currentWeatherId}@2x.png"<img>`
         cityName.innerHTML = `${json.city["name"]}`
-        tempCelsius.innerHTML = `<span> ${tempertureCurrentDay}℃ Min: ${minTemperature}℃</span>  <span> Max: ${maxTemperature}℃</span> ` // The weather icon will be changed depending on time and is affected by an function that will trigger and if else statement(its its cloudy === this picture etc.)
-        sunRise.innerHTML = `Sunrise: ${dateRise} AM `
-        sunSet.innerHTML = `Sunset: ${dateSet} PM` 
+        tempCelsius.innerHTML = `<span> ${tempertureCurrentDay}℃ Min: ${minTemperature}℃</span>  <span> Max: ${maxTemperature}℃</span> ` // The weather icon will be changed depending on time and is affected by an function that will trigger and if else statement(its its cloudy === this picture etc.) 
 
 
     // for (let i = 0; i < dateFiveDays.length && i < temperatureFiveDays.length && i < fiveDaysId.length; i++) {
@@ -110,35 +112,34 @@ const SthlmTemp = () => {
     
     //Styling of the body color depending on the current weather
 
-    switch (currentWeather) {
-      case 'Clouds': {
-        body.style.backgroundImage = "url('')"
-      }
-        break;
-      case 'Snow': {
-        break;
-      }
+    if (tempertureCurrentDay < 0 && tempertureCurrentDay > -10) {
+        funnyText.innerHTML = `Dress well, it is freezing out there!`
+        body.style.background = "linear-gradient(0deg, rgba(34,92,195,1) 0%, rgba(162,200,210,1) 100%)"
+    } else if (tempertureCurrentDay>0 && tempertureCurrentDay<10){
+      funnyText.innerHTML = `It is a bit chilly, think of a warmer coat!`
+        body.style.background = "linear-gradient(0deg, rgba(34,188,195,1) 0%, rgba(195,209,213,1) 100%);"
     }
 
-    const accordionQuestion = document.querySelectorAll('.accordion-question');
-
-    allCities.forEach(cityName => {
-      cityName.addEventListener('click', () => {
-        cityName.classList.toggle('clicked')
-        const info =cityName.nextElementSibling;
-        if(cityName.classList.contains('clicked')) {
-          info.style.height = info.scrollHeight + 'px';
-        }
-        else {
-          info.style.height = '0';
-        }
-    })
-    })
-    
+    // allCities.forEach(cityName => {
+    //   cityName.addEventListener('click', () => {
+    //     cityName.classList.toggle('clicked')
+    //     const info =cityName.nextElementSibling;
+    //     if(cityName.classList.contains('clicked')) {
+    //       info.style.height = info.scrollHeight + 'px';
+    //     }
+    //     else {
+    //       info.style.height = '0';
+    //     }
+    // })
+    // })
     })
 }
+SthlmTemp('Stockholm')
+selectCity.addEventListener('change', () => {
+  dayOne.innerHTML = '';
+  SthlmTemp(selectCity.value)
+}) 
 
-SthlmTemp()
 
 
 
